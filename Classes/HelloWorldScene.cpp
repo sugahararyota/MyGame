1d2b1b75e453eb79b879f08584969dc90e85723e
sugahararyota <s.ryota072@gmail.com>
2019-05-25 12:03:44 +0900

    スプライト変更

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -101,20 +101,11 @@ bool HelloWorld::init()
         this->addChild(label, 1);
     }
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+   
 
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
-    }
+	Sprite* sprite = Sprite::create("neko.png");
+	this->addChild(sprite);
+
     return true;
 }
 
